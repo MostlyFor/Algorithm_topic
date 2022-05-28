6a265cea7ad4dfe74776a9b94cba545b281702a1
MostlyFor <106165619+MostlyFor@users.noreply.github.com>
2022-05-28 13:28:58 +0900

    Add files via upload

---
+++ day 1 - leetcode 268.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        // 배열 크기 만큼 서로 다른 숫자가 있음. 0 ~ n 중 n개 있음.
+        // 0 ~ n 중 없는 숫자 return
+        // space O(1), time O(n)에 해결해야함.
+        
+        int n = nums.size();
+        
+        int sum = (n*n + n)/2;
+        int missing_sum = 0;
+        
+        for(auto i : nums)
+            missing_sum+=i;
+        
+        
+        return sum - missing_sum;
+    }
+};
