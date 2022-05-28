0ac09bfa8bc91db4e57de8eb5b5da52523e54be4
MostlyFor <106165619+MostlyFor@users.noreply.github.com>
2022-05-28 13:48:20 +0900

    Update day 1 - leetcode 268.cpp

--- day 1 - leetcode 268.cpp
+++ day 1 - leetcode 268.cpp
@@ -21,3 +21,24 @@ public:
         return sum - missing_sum;
     }
 };
+
+
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        // 배열 크기 만큼 서로 다른 숫자가 있음. 0 ~ n 중 n개 있음.
+        // 0 ~ n 중 없는 숫자 return
+        // space O(1), time O(n)에 해결해야함.
+        
+        int n = nums.size();
+        
+        int xor_ = 0;
+
+        for(int i=0; i<n; i++)
+            xor_^=nums[i]^i;
+        
+        
+        return xor_^n;;
+    }
+};
