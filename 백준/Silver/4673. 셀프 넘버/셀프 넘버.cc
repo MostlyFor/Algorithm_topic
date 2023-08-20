c149eface4e554b28b6d96c9ce4bd511c7a6fb0b
MostlyFor <106165619+MostlyFor@users.noreply.github.com>
2023-08-20 09:19:46 +0900

    [Silver V] Title: 셀프 넘버, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <set>
+#include <tuple>
+#include <map>
+
+using namespace std;
+
+
+//시간복잡도 O(1) : 10000
+
+bool is_not_self[10036];
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    for (int i = 1; i < 10000; i++) {
+        if (!is_not_self[i]) {
+            cout << i << '\n';
+        }
+
+        // i번을 생성자로 계산
+        int gn = i;
+        int k = i;
+        while (k) {
+            gn += k % 10;
+            k /= 10;
+        }
+        is_not_self[gn] = true;
+    }
+    
+    return 0;
+}
