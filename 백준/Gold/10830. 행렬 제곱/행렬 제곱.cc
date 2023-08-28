0688b49b8a0f21a9c25313c8c19c7f3c6e21a329
MostlyFor <106165619+MostlyFor@users.noreply.github.com>
2023-08-28 10:04:21 +0900

    [Gold IV] Title: 행렬 제곱, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/10830. 행렬 제곱/행렬 제곱.cc
+++ 백준/Gold/10830. 행렬 제곱/행렬 제곱.cc
@@ -9,8 +9,6 @@
 #include <map>
 using namespace std;
 
-
-map <long long, vector<vector<int>> > m;
 vector<vector<int>> matrix;
 int n;
 long long b;
@@ -32,18 +30,6 @@ vector<vector<int>> dot(vector<vector<int>> mat1, vector<vector<int>> mat2) {
 }
 
 
-vector<vector<int>> f(vector<vector<int>> mat, long long k) {
-    if (k == 1) return mat;
-    if (m.find(k / 2) == m.end()) {
-        m[k / 2] = f(mat, k / 2);
-    }
-    if (m.find(k - k / 2) == m.end()) {
-        m[k - k / 2] = f(mat, k - k / 2);
-    }
-    return dot(m[k / 2], m[k - k / 2]);
-}
-
-
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
@@ -51,21 +37,29 @@ int main() {
     cin >> n >> b;
 
     vector<vector<int>> matrix;
+    vector<vector<int>> ans(n,vector<int>(n,0));
 
     for (int i = 0; i < n; i++) {
         vector<int> tmp;
         for (int j = 0; j < n; j++) {
             int t; cin >> t;
             tmp.push_back(t);
+            ans[i][i] = 1;
         }
         matrix.push_back(tmp);
     }
 
-    auto matrix_new = f(matrix, b);
+    while (b != 0) {
+        if (b % 2 == 1) {
+            ans = dot(ans, matrix);
+        }
+        matrix = dot(matrix, matrix);
+        b /= 2;
+    }
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++)
-            cout << matrix_new[i][j]%1000 << ' ';
+            cout << ans[i][j]%1000 << ' ';
         cout << '\n';
     }
 
