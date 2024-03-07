c98b06ce418ad96ecf4de89a9c2871b553412391
MostlyFor <106165619+MostlyFor@users.noreply.github.com>
2024-03-07 18:03:15 +0900

    Time: 480 ms (64.39%), Space: 11.7 MB (42.08%) - LeetHub

---
+++ 0079-word-search/0079-word-search.cpp
@@ -0,0 +1,54 @@
+class Solution {
+    int r,c;
+    bool pos;
+    vector<vector<char>> board;
+    string word;
+    int dx[4] = {0,0,1,-1};
+    int dy[4] = {1,-1,0,0};
+    
+public:
+    bool InRange(int x, int y){
+        return 0 <= x && 0<= y && x <r && y<c;
+    }
+    
+    // board[x][y] == word[cnt] 인지 확인 (현재 시점 확인)
+    void bc(int x, int y, int cnt, vector<vector<bool>>& visit){
+        if(board[x][y] != word[cnt]) return;
+        
+        visit[x][y] = true;
+        
+        cnt++;
+        
+        if(cnt == word.size()) pos = true;
+        
+        
+        for(int dir = 0; dir <4; dir++){
+            if(InRange(x+dx[dir], y+dy[dir]) && visit[x+dx[dir]][y+dy[dir]] == false){
+                bc(x+dx[dir], y+dy[dir], cnt, visit);
+            }
+        }
+        visit[x][y] = false;
+    }
+    
+    
+    bool exist(vector<vector<char>>& board2, string word2) {
+        
+        r = board2.size();
+        c = board2[0].size();
+        word = word2;
+        
+        board.resize(r,vector<char>(c,' '));
+        for(int i=0; i<r; i++)
+            for(int j=0; j<c; j++) board[i][j] = board2[i][j];
+        
+        for(int i=0; i<r; i++){
+            for(int j=0; j<c; j++){
+                vector<vector<bool>> visit(r,  vector<bool>(c,0));
+                bc(i, j, 0, visit);
+            }
+        }
+        
+        
+        return pos;
+    }
+};
