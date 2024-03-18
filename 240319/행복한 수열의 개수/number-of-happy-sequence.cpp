10d51b67f3aa254c8d364af3d3d7f2d8c71d0521
MostlyFor <106165619+MostlyFor@users.noreply.github.com>
2024-03-19 08:54:55 +0900

    Title: 행복한 수열의 개수, Time: 15ms, Memory: 0MB, Status: Passed - CodeTree

--- 240319/행복한 수열의 개수/number-of-happy-sequence.cpp
+++ 240319/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -6,23 +6,25 @@ int n,m;
 
 bool is_happy(bool is_row, int col) {
 	int seq = 1;
-	for (int i = 1; i < n; i++) {
-		int cnt = 1;
 
-		if (is_row) {
-			int pre = board[col][0];
+	if (is_row) {
+		int pre = board[col][0];
+		int cnt = 1;
+		for (int i = 1; i < n; i++) {
 			if (pre == board[col][i]) cnt++;
 			else cnt = 1;
 			seq = max(cnt, seq);
 			pre = board[col][i];
 		}
-		else {
-			int pre = board[0][col];
+	}
+	else {
+		int pre = board[0][col];
+		int cnt = 1;
+		for (int i = 1; i < n; i++) {
 			if (pre == board[i][col]) cnt++;
 			else cnt = 1;
 			seq = max(cnt, seq);
 			pre = board[i][col];
-
 		}
 	}
 	return seq >= m;
