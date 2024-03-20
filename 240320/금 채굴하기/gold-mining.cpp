1490c01348731aa9e99da45b011d33fd089ac5a8
MostlyFor <106165619+MostlyFor@users.noreply.github.com>
2024-03-20 14:03:40 +0900

    Title: 금 채굴하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240320/금 채굴하기/gold-mining.cpp
+++ 240320/금 채굴하기/gold-mining.cpp
@@ -11,8 +11,8 @@ int get_gold(int x, int y, int level) {
 
 	for (int i = x - level; i < x + level; i++) {
 		for (int j = y - level; j < y + level; j++) {
-			if (0 > i || 0 > j || n <= i || m <= j) continue;
-			if (abs(i - x) + abs(j - y) > level)continue;
+			if (0 > i || 0 > j || n <= i || n <= j) continue;
+			if (abs(i - x) + abs(j - y) > level) continue;
 			total += board[i][j];
 		}
 	}
@@ -23,19 +23,22 @@ int get_gold(int x, int y, int level) {
 // x ,y 에서 채굴 시작
 int mining(int x, int y) {
 	// k에 따른 마름모를 최대로 늘려가면서 한번 해보기
-	int max_profit = board[x][y];
-	int profit = board[x][y]; 
+	int max_gold = board[x][y];
+	int gold = board[x][y]; 
+	int profit = board[x][y];
 	int level = 0;
-	while (profit > 0) {
+	while (level < n) {
 		// - 채굴비용 + 채굴했을 때 얻는 금액
 		level++;
 		int cost = level * level + (level + 1) * (level + 1);
-		profit = -cost + get_gold(x, y, level);
-		max_profit = max(max_profit, profit);
+		gold = get_gold(x, y, level);
+		profit = - cost + m * gold;
+		if (profit < 0) continue;
+		max_gold = max(max_gold, gold);
 	}
 
 
-	return max_profit;
+	return max_gold;
 }
 
 
@@ -43,7 +46,7 @@ int main() {
 	cin >> n >> m;
 
 	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
+		for (int j = 0; j < n; j++) {
 			cin >> board[i][j];
 		}
 	}
@@ -52,7 +55,7 @@ int main() {
 	int level = 0;
 
 	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
+		for (int j = 0; j < n; j++) {
 			ans = max(ans, mining(i, j));
 		}
 	}
