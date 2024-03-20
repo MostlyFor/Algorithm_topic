2680fe28379d0fcdcdb1126aa79842c07ea1fa1c
MostlyFor <106165619+MostlyFor@users.noreply.github.com>
2024-03-20 22:34:59 +0900

    Title: 스승의 은혜 3, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240320/스승의 은혜 3/the-grace-form-teacher-3.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n, b;
+int ans;
+vector<int> arr[2];
+vector<int> present;
+int main() {
+	cin >> n >> b;
+	
+	for (int i = 0; i < n; i++) {
+		int x, y; cin >> x >> y;
+		arr[0].push_back(x);
+		arr[1].push_back(y);
+		present.push_back(x + y);
+	}
+
+	for (int i = 0; i < n; i++) {
+		int people = 0;
+		int cost = 0;
+		vector<int> tmp(present.size());
+		copy(present.begin(), present.end(), tmp.begin());
+		tmp[i] -= arr[0][i];
+			
+
+		sort(tmp.begin(), tmp.end());
+
+		// people 번째 학생의 선물을 줄 수 있음
+		while (b > cost + tmp[people]) {
+			cost += tmp[people++];
+		}
+
+		ans = max(ans, people);
+	}
+
+
+	cout << ans;
+	return 0;
+}
