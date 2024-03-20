e331e214c6b9ae6bf5ecf0ea7246a57e8e76812b
MostlyFor <106165619+MostlyFor@users.noreply.github.com>
2024-03-20 13:30:15 +0900

    Title: 트로미노, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240320/트로미노/tromino.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n, m;
+int board[200][200];
+int main() {
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+			cin >> board[i][j];
+
+	int ans = 0;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j + 2 < m; j++) {
+			int cnt = board[i][j] + board[i][j + 1] + board[i][j + 2];
+			ans = max(ans, cnt);
+		}
+	}
+
+	for (int i = 0; i+1 < n; i++) {
+		for (int j = 0; j + 1 < m; j++) {
+			int cnt = board[i][j] + board[i + 1][j] + board[i][j + 1];
+			ans = max(ans, cnt);
+		}
+	}
+
+	cout << ans;
+	return 0;
+}
