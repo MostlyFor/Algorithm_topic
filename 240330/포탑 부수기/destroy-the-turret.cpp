d692a4ce6fbe1218169e84c6bcdcda646ca96990
MostlyFor <106165619+MostlyFor@users.noreply.github.com>
2024-03-30 18:25:49 +0900

    Title: 포탑 부수기, Time: 79ms, Memory: 0MB, Status: Passed - CodeTree

--- 240330/포탑 부수기/destroy-the-turret.cpp
+++ 240330/포탑 부수기/destroy-the-turret.cpp
@@ -9,14 +9,14 @@ int n, m, k; // n,m 격자, k는 턴 수 (만약 부서지지 않으 포탑이 1
 
 int board[10][10]; // 포탑 공격력
 int btime[10][10]; // 최근 공격 시점
+
+// 초기화 필요한 항목
 int dist[10][10]; // att와 def 거리 측정할 수 있게!
 bool eff[10][10]; // 영향 받은 포탑 기록
-
-
-int left_tower = 0; // 남아있는 포탑 수
 int turn = 0;
 pair<int, int> att; // 공격자
 pair<int, int> def; // 공격 대상자
+vector<pair<int, int>> lroute; // 레이저 경로
 
 // 우선순위 우/하/좌/상
 int dx[4] = { 0,1,0,-1 };
@@ -82,18 +82,14 @@ void set_att() {
     att = { get<2>(arr.back()) - get<3>(arr.back()) , get<3>(arr.back())};
 }
 
-// att -> def 경로 확인
+// att -> def 경로 확인 bfs
 void get_route() {
-    for (int i = 0; i < n; i++) for (int j = 0; j < m; j++) dist[i][j] = 0;
-
-
     queue<pair<int, int>> q;
     q.push(att);
     dist[att.first][att.second] = 1;
 
     while (q.size()) {
         auto k = q.front(); q.pop();
-
 
         for (int i = 0; i < 4; i++) {
             int nx = k.first + dx[i];
@@ -112,37 +108,44 @@ void get_route() {
     }
 }
 
+
+
+// dfs
+void get_route2(int x, int y, vector<pair<int,int>> route) {
+
+    if (lroute.size() != 0) return;
+
+    if (make_pair(x, y) == def) {
+        route.push_back(def);
+        lroute = route;
+        return;
+    }
+    
+    for (int i = 0; i < 4; i++) {
+        int nx = (x + dx[i] + n) % n;
+        int ny = (y + dy[i] + m) % m;
+        // 만약 다음에 갈 곳이 거리 1 이상 차이가 난다면?
+        if (dist[nx][ny] != dist[x][y] + 1) continue;
+        route.push_back({ x,y });
+        get_route2(nx, ny, route);
+        route.pop_back();
+    }
+}
+
 // 2. 레이저 공격
 void laser() {
-    // dist 기반으로 경로 찾기
+
+    // dfs 와 dist 기반으로 경로 찾기
     vector<pair<int, int>> laser_route;
-    laser_route.push_back(def);
-    int hx = def.first;
-    int hy = def.second;
-    while (!(hx == att.first && hy == att.second)) {
-        // 우선순위 반대로 돌면서 해결하기
-        for (int i = 3; i >= 0; --i) {
-            int nx = hx + dx[i];
-            int ny = hy + dy[i];
-            nx = (nx + n) % n;
-            ny = (ny + m) % m;
 
-            if (dist[nx][ny] == dist[hx][hy] - 1) {
-                hx = nx; hy = ny;
-                laser_route.push_back({ hx,hy });
-                break;
-            }
-        }
-    }
-
-
-    // 경로 체크
-    //for(auto aa : laser_route) cout << aa.first << ' ' << aa.second <<'\n';
+    get_route2(att.first, att.second, laser_route);
 
     // 경로에 해당하는 애들 공격
-    for (int i = 0; i < laser_route.size() - 1; i++) {
-        auto aa = laser_route[i];
-        if (i == 0) board[aa.first][aa.second] -= board[att.first][att.second];
+    for (int i = 0; i < lroute.size(); i++) {
+       
+        auto aa = lroute[i];
+        if (aa == att) continue;
+        if (i == lroute.size()-1) board[aa.first][aa.second] -= board[att.first][att.second];
         else board[aa.first][aa.second] -= board[att.first][att.second] / 2;
 
         eff[aa.first][aa.second] = true;
@@ -157,13 +160,23 @@ void machine() {
         for (int j = -1; j <= 1; j++) {
             int nx = (hx + i + n) % n;
             int ny = (hy + j + m) % m;
-
+            if (make_pair(nx, ny) == att) continue;
             if (nx == hx && hy == ny) board[nx][ny] -= board[att.first][att.second];
             else if (board[nx][ny] != 0) board[nx][ny] -= board[att.first][att.second] / 2;
 
             eff[nx][ny] = true;
         }
     }
+}
+
+
+// 초기화
+void init() {
+    btime[att.first][att.second] = turn;
+    for (int i = 0; i < n; i++) for (int j = 0; j < m; j++) eff[i][j] = 0;
+    for (int i = 0; i < n; i++) for (int j = 0; j < m; j++) dist[i][j] = 0;
+    turn++;  att = { 0,0 }; def = { 0,0 };
+    lroute.clear();
 }
 
 
@@ -185,34 +198,32 @@ int main() {
 
 
     while (k--) {
-        turn++;
+        // 매 턴마다 변수 초기화
+        init();
 
-        // 영향받은 애 초기화
-        for (int i = 0; i < n; i++) for (int j = 0; j < m; j++) eff[i][j] = 0;
-
-        // 1. 공격자 선정 + 공격 대상자 선정  + att와 def 초기화
+        // 1. 공격자 선정 + 공격 대상자 선정
         set_att();
 
-        // if() 만약 부서지지 않은 포탑이 하나라면 게임 끝
+        // 만약 부서지지 않은 포탑이 하나라면 게임 끝
         if (att == def) break;
 
-        // 수비자 공격력 추가
+        // 공격력 추가
         board[att.first][att.second] += n + m;
 
-        // att -> def 갈 수 있는지? + dist 초기화
+        // att -> def 경로 구하기
         get_route();
 
         // 2. 포탑 공격
         if (dist[def.first][def.second]) laser(); // 레이져
         else machine(); // 포탑공격
 
-        // 3. 부서진 포탑 처리 + 포탑 정비 + eff 초기화
+        // 3. 부서진 포탑 처리 + 포탑 정비
         after_att();
 
-        // 최근 공격 시간 갱신 time 초기화
-        btime[att.first][att.second] = turn;
+
     }
 
     output();
+
     return 0;
 }
