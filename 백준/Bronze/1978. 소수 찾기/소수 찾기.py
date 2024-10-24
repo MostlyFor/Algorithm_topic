63dc4112e265df77f5fc34d22f6e338f8a8ae129
Younghoon Hong <106165619+MostlyFor@users.noreply.github.com>
2024-10-24 18:54:20 +0900

    [Bronze II] Title: 소수 찾기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -3,19 +3,12 @@ n = int(input())
 arr = list(map(int, input().split()))
 
 is_prime = [1] * 1001
-
 is_prime[1] = 0
 
-for i in range(2,1001):
-    cnt = i
-    while(i * cnt <= 1000):
-        is_prime[i*cnt] = 0
-        cnt+=1
+for i in range(2,int(1000**0.5)+1):
+    if is_prime[i]:
+        for j in range(i*i, 1001, i):
+            is_prime[j] = 0
 
-ans = 0
-for x in arr:
-    if is_prime[x]:
-        ans+=1
-
+ans = sum(1 for x in arr if is_prime[x])
 print(ans)
-    
