ecb549e79b6b97d376d4e873f54683b8f8c280b3
Younghoon Hong <106165619+MostlyFor@users.noreply.github.com>
2024-11-01 16:18:03 +0900

    Time: 0 ms (100.00%), Space: 17 MB (27.84%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
@@ -3,8 +3,10 @@ class Solution:
         ans = 0
         n = len(mat[0])
         for i in range(n):
-            for j in range(n):
-                if i==j: ans += mat[i][j]
-                elif (i+j)==n-1: ans += mat[i][j]
+            ans += mat[i][i]
+            ans += mat[i][n-1-i]
+        
+        if n % 2 != 0:
+             ans -= mat[n // 2][n // 2]
 
         return ans
