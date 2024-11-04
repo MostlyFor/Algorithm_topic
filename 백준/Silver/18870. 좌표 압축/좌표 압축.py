a3e1d66440adf01eaaf462625f9f333aa1a9b06f
Younghoon Hong <106165619+MostlyFor@users.noreply.github.com>
2024-11-04 09:41:21 +0900

    [Silver II] Title: 좌표 압축, Time: 1884 ms, Memory: 154784 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -5,15 +5,14 @@ n = int(input().rstrip())
 
 arr = list(map(int, input().rstrip().split()))
 
-arr_sorted = sorted(arr)
+arr_sorted = sorted(set(arr))
 
 hmap = {} # 나보다 작은 원소의 개수 저장
 
 hmap[arr_sorted[0]] = 0 # 초기값
 
 for i in range(1, len(arr_sorted)):
-    if arr_sorted[i] > arr_sorted[i-1]:
-        hmap[arr_sorted[i]] = len(hmap)
+    hmap[arr_sorted[i]] = i
 
 for x in arr:
     print(hmap[x], end=' ')
