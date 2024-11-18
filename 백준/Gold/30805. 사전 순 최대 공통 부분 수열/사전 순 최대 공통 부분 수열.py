8bf34944a8679aa0181d64f1be5088b8cc9c32bb
Younghoon Hong <106165619+MostlyFor@users.noreply.github.com>
2024-11-19 08:07:26 +0900

    [Gold IV] Title: 사전 순 최대 공통 부분 수열, Time: 100 ms, Memory: 109108 KB -BaekjoonHub

--- 백준/Gold/30805. 사전 순 최대 공통 부분 수열/사전 순 최대 공통 부분 수열.py
+++ 백준/Gold/30805. 사전 순 최대 공통 부분 수열/사전 순 최대 공통 부분 수열.py
@@ -3,38 +3,28 @@ arr1 = list(map(int, input().split()))
 m = int(input())
 arr2 = list(map(int, input().split()))
 
-ans = []
-
-
-def get_max_element(arr1, arr2):
+def get_max_element(arr1, arr2, ans):
     if len(arr1) == 0 or len(arr2) == 0:
-        return 0
+        return ans
     
-
     tmp1 = max(arr1)
     tmp2 = max(arr2)
+    index1 = arr1.index(tmp1)
+    index2 = arr2.index(tmp2)
 
     if tmp1 > tmp2:
         arr1.remove(tmp1)
-        return get_max_element(arr1, arr2)
+        return get_max_element(arr1, arr2, ans)
     
     elif tmp1 < tmp2:
         arr2.remove(tmp2)
-        return get_max_element(arr1, arr2)
+        return get_max_element(arr1, arr2, ans)
     
     else:
-        index1 = arr1.index(tmp1)
-        index2 = arr2.index(tmp2)
-        del arr1[:index1 + 1]
-        del arr2[:index2 + 1]
+        ans.append(tmp1)
+        return get_max_element(arr1[index1+1:], arr2[index2+1:], ans)
 
-        return tmp1
-
-
-
-while a := get_max_element(arr1, arr2):
-    ans.append(a)
-
+ans = get_max_element(arr1, arr2, [])
 
 print(len(ans))
 print(*ans)
