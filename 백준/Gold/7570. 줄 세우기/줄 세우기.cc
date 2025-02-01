36edb37dd7b678d0f94be213b3241cc565419c15
Younghoon Hong <106165619+MostlyFor@users.noreply.github.com>
2025-02-02 07:10:06 +0900

    [Gold II] Title: 줄 세우기, Time: 396 ms, Memory: 9832 KB -BaekjoonHub

--- 백준/Gold/7570. 줄 세우기/줄 세우기.cc
+++ 백준/Gold/7570. 줄 세우기/줄 세우기.cc
@@ -2,25 +2,19 @@
 using namespace std;
 
 int n;
-int expected[1000002]; // expected[i] = x : 다음 수로 i를 기대하는 인덱스 x 
-int arr[1000000];
-int inc[1000000]; 
+int arr[1000001];
+int dp[1000001];
+
 int main(){
     cin >> n;
-    for(int i=0; i<n; i++){
+    for(int i=1; i<=n; i++){
         cin >> arr[i];
-        expected[arr[i]+1] = i;
-        inc[i] = 1;
     }
     
     int ans = 0;
-    for(int i=0; i<n; i++){
-        if(arr[i] == 1) continue;
-        int idx = expected[arr[i]]; // arr[i]를 기대하고 있던 idx
-        if(i < idx) continue; // 앞에 있다면 제거
-        inc[idx]++;
-        expected[arr[i]+1] = idx;
-        ans = max(ans,inc[idx]);
+    for(int i=1; i<=n; i++){
+        dp[arr[i]] = dp[arr[i]-1] + 1;
+        ans = max(dp[arr[i]], ans);
     }
     
      cout << ((ans == 0) ? n-1 : n - ans) << '\n';
