5bb3df861c20d23358daf4a721166879c3808f86
Younghoon Hong <106165619+MostlyFor@users.noreply.github.com>
2025-02-02 06:52:46 +0900

    [Gold II] Title: 줄 세우기, Time: 404 ms, Memory: 13740 KB -BaekjoonHub

--- 백준/Gold/7570. 줄 세우기/줄 세우기.cc
+++ 백준/Gold/7570. 줄 세우기/줄 세우기.cc
@@ -1,50 +1,29 @@
 #include <iostream>
-#include <vector>
-#include <queue>
-#include <algorithm>
-#include <string>
-#include <queue>
-#include <set>
-#include <tuple>
-#include <map>
-
 using namespace std;
 
-
-// 시간복잡도 O(N)
-// 공간복잡도 O(N)
-
-int dp[1000000];
-int pos[1000000];
-
-int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
+int n;
+int expected[1000002]; // expected[i] = x : 다음 수로 i를 기대하는 인덱스 x 
+int arr[1000000];
+int inc[1000000]; 
+int main(){
+    cin >> n;
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+        expected[arr[i]+1] = i;
+        inc[i] = 1;
+    }
+    
     int ans = 0;
-
-    vector<int> arr;
-    
-    int n; cin >> n;
-    
-    for (int i = 0; i < n; i++) {
-        int tmp; cin >> tmp;
-        pos[tmp] = i;
-        arr.push_back(tmp);
+    for(int i=0; i<n; i++){
+        if(arr[i] == 1) continue;
+        int idx = expected[arr[i]]; // arr[i]를 기대하고 있던 idx
+        if(i < idx) continue; // 앞에 있다면 제거
+        inc[idx]++;
+        expected[arr[i]+1] = idx;
+        ans = max(ans,inc[idx]);
     }
-
-
-    int ans_max = 0;
-
-    for (int i = 0; i < n; i++) {
-        dp[i] = 1;
-        int h = arr[i];
-        // 내가 찾아야하는 h-1값이 내 이전 인덱스에 존재
-        if (i > pos[h - 1]) dp[i] = dp[pos[h - 1]] + 1;
-        ans_max = max(dp[i], ans_max);
-    }
-
-
-    cout << n - ans_max;
-
+    
+     cout << ((ans == 0) ? n-1 : n - ans) << '\n';
+    
     return 0;
 }
