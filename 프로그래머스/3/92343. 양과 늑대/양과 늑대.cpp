c897d1b2eeca92056432e061fb07ef79974b41b3
Younghoon Hong <106165619+MostlyFor@users.noreply.github.com>
2025-05-19 21:43:45 +0900

    [level 3] Title: 양과 늑대, Time: 1.19 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.cpp
@@ -0,0 +1,79 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> adj[18]; 
+int pos_node[18];
+int visit[18];
+int answer = 0;
+int n;
+
+void dfs(vector<int>& info, int h, int s, int w, vector<int>& route){
+    if(s <= w) return; // 전부 다 돌았거나, 늑대가 더 많으면 종료
+    answer = max(answer, s); // 값 초기화
+//     if(answer == 4) {
+//         for(auto x : route) cout << x << ' ';
+//         cout << '\n';
+        
+//         for(auto x : pos_node) cout << x << ' ';
+//         cout << '\n';
+//     }
+    
+    // 방문 가능한 edge들을 모두 방문하기
+    for(int i=0; i<n; i++){
+        // 만약 방문 가능하면 방문
+        if(visit[i] == 0 && pos_node[i]) {
+            
+            if(info[i] == 0){
+                visit[i] = 1;
+                vector<int> new_pos;
+                for(auto k : adj[i]) {
+                    if(pos_node[k] == 0) new_pos.push_back(k);
+                    pos_node[k] = 1;
+                }
+                route.push_back(i);
+                dfs(info, i, s+1, w, route);
+                visit[i] = 0;
+                route.pop_back();
+                for(auto k : new_pos) pos_node[k] = 0;
+            }
+            
+            else if(info[i] == 1){
+                visit[i] = 1;
+                
+                vector<int> new_pos;
+                for(auto k : adj[i]) {
+                    if(pos_node[k] == 0) new_pos.push_back(k);
+                    pos_node[k] = 1;
+                }
+                
+                route.push_back(i);
+                dfs(info, i, s, w+1, route);
+                visit[i] = 0;
+                route.pop_back();
+                for(auto k : new_pos) pos_node[k] = 0;
+            }
+        }
+    }
+}
+
+int solution(vector<int> info, vector<vector<int>> edges) {
+    n = info.size();
+    // 양이 늑대에게 잡아먹히지 않도록 하면서 최대한 많은 수의 양 모으기
+    
+    for(int i=0; i<edges.size(); i++){
+        int a = edges[i][0];
+        int b = edges[i][1];
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+    
+    visit[0] = 1;
+    for(auto n : adj[0]) pos_node[n] = 1;
+    vector<int> tmp = {0};
+    dfs(info, 0, 1, 0, tmp);
+    
+    return answer;
+}
