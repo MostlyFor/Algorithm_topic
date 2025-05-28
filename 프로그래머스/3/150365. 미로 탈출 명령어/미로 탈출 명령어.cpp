1b9ff0c9eeb733fbbffe2ad9b80689d4e00acc0a
Younghoon Hong <106165619+MostlyFor@users.noreply.github.com>
2025-05-28 22:03:19 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 6.19 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.cpp
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.cpp
@@ -5,6 +5,8 @@
 using namespace std;
 string answer = "";
 
+// O(4K)
+
 // 1.d , 2.l , 3. r, 4. u
 
 int dx[4] = {1, 0, 0, -1};
@@ -15,8 +17,7 @@ void f(string ans, int n, int m, int x, int y, int r, int c, int k){
     if(answer != "") return; // 1. 정답이 나왔으면 끝
     int dist = abs(r-x) + abs(c-y); // 남은거리
     if(dist > k) return; // 2. 못가는 거리면 끝
-    // cout << dist << ' ' << k - dist << ' ';
-    if( (k - dist) % 4 != 0 && (k - dist) % 2 != 0) return; // 3. 남은 거리가 2의 배수이거나 4의 배수가 아니면 끝
+    if((k - dist) % 2 != 0) return; // 3. 남은 거리가 2의 배수이거나 4의 배수가 아니면 끝
     
     if(k == 0){
         answer = ans;
@@ -29,14 +30,11 @@ void f(string ans, int n, int m, int x, int y, int r, int c, int k){
         if(nx < 1 || ny < 1 || nx > n || ny > m) continue;
         f(ans + direction[dir], n, m, nx, ny, r, c, k-1);
     }
-    
-    cout << '?' << '\n';
+
 }
 
 
 string solution(int n, int m, int x, int y, int r, int c, int k) {
-
-    
     f("", n, m, x, y, r, c, k);
     if(answer == "") answer = "impossible";
     return answer;
