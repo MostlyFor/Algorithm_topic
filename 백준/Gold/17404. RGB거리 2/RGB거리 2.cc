93e101584964a6a02057f7f3fc007ee62f7b489a
Younghoon Hong <106165619+MostlyFor@users.noreply.github.com>
2025-06-23 22:27:10 +0900

    [Gold IV] Title: RGB거리 2, Time: 0 ms, Memory: 2068 KB -BaekjoonHub

--- 백준/Gold/17404. RGB거리 2/RGB거리 2.cc
+++ 백준/Gold/17404. RGB거리 2/RGB거리 2.cc
@@ -1,9 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int dp[1001][3];
-int dp1[1001][3];
-int dp2[1001][3];
+int dp[1001][3][3];
 int arr[1001][3]; // 색칠 비용 정보
 
 int main(){
@@ -13,37 +11,32 @@ int main(){
     for(int i=1; i<=n; i++){
         for(int j=0; j<3; j++){
             cin >> arr[i][j];
-            dp[i][j] = 1e9;
-            dp1[i][j] = 1e9;
-            dp2[i][j] = 1e9;
+            for(int k=0; k<3; k++)
+            dp[i][j][k] = 1e9;
         }
     }
     
     
     // 1. a로 시작하는 경우
-    dp[1][0] = arr[1][0];
+    dp[1][0][0] = arr[1][0]; // 0 시작
+    dp[1][1][1] = arr[1][1]; // 1 시작
+    dp[1][2][2] = arr[1][2]; // 2 시작
+    
     for(int i=2; i<=n; i++){
-        dp[i][0] = min(dp[i-1][1] + arr[i][0], dp[i-1][2] + arr[i][0]);
-        dp[i][1] = min(dp[i-1][0] + arr[i][1], dp[i-1][2] + arr[i][1]);
-        dp[i][2] = min(dp[i-1][0] + arr[i][2], dp[i-1][1] + arr[i][2]);
+        for(int k=0; k<3; k++){
+            dp[i][0][k] = min(dp[i-1][1][k] + arr[i][0], dp[i-1][2][k] + arr[i][0]);
+            dp[i][1][k] = min(dp[i-1][0][k] + arr[i][1], dp[i-1][2][k] + arr[i][1]);
+            dp[i][2][k] = min(dp[i-1][0][k] + arr[i][2], dp[i-1][1][k] + arr[i][2]);
+        }
     }
     
-    dp1[1][1] = arr[1][1];
-    for(int i=2; i<=n; i++){
-        dp1[i][0] = min(dp1[i-1][1] + arr[i][0], dp1[i-1][2] + arr[i][0]);
-        dp1[i][1] = min(dp1[i-1][0] + arr[i][1], dp1[i-1][2] + arr[i][1]);
-        dp1[i][2] = min(dp1[i-1][0] + arr[i][2], dp1[i-1][1] + arr[i][2]);
+    int answer = 1e9;
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++){
+            if(i == j) continue;
+            answer = min(answer, dp[n][i][j]);
+        }
     }
-    
-    dp2[1][2] = arr[1][2];
-    for(int i=2; i<=n; i++){
-        dp2[i][0] = min(dp2[i-1][1] + arr[i][0], dp2[i-1][2] + arr[i][0]);
-        dp2[i][1] = min(dp2[i-1][0] + arr[i][1], dp2[i-1][2] + arr[i][1]);
-        dp2[i][2] = min(dp2[i-1][0] + arr[i][2], dp2[i-1][1] + arr[i][2]);
-    }
-    
-    
-    int answer = min(dp[n][1], min(min(dp[n][2], dp1[n][0]), min(dp1[n][2], min(dp2[n][0], dp2[n][1]))));
     cout << answer;
     
     return 0;
