0a5a0de0c61e639f8235422d6de2455f1f6ded86
Younghoon Hong <106165619+MostlyFor@users.noreply.github.com>
2025-08-14 07:52:56 +0900

    [Gold III] Title: 텀 프로젝트, Time: 432 ms, Memory: 2932 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.cc
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.cc
@@ -1,38 +1,32 @@
 #include <iostream>
 #include <vector>
-#include <unordered_map>
+#include <queue>
 using namespace std;
 
 int n;
-unordered_map<int,int> visit; // 이전 사이클에 방문했던 노드
-unordered_map<int,int> is_in_cycle; // 사이클인지?
-int pointer[100001];
-
-void f(int st, vector<int>& arr, unordered_map<int,int>& tmp){
-    arr.push_back(st);
-    if(visit.find(st) != visit.end()) return; // 함수의 종료 조건 2. 이전에 방문했었던 노드
-    if(is_in_cycle.find(st) != is_in_cycle.end()) return; // 함수의 종료 조건 1. 사이클 탐지
-    tmp[st]++;
-    if(tmp[st] == 2) is_in_cycle[st]++;
-    f(pointer[st], arr, tmp);
-}
+int pointer[100001]; 
+int indegree[100001];
 
 
 void solve(){
+    queue<int> q; // 차수만큼
     cin >> n;
-    is_in_cycle.clear();
-    visit.clear();
-    for(int i=1; i<=n; i++) cin >> pointer[i];
+    for(int i=1; i<=n; i++) indegree[i] = 0;
+    for(int i=1; i<=n; i++) {
+        cin >> pointer[i];
+        indegree[pointer[i]]++;
+    }
+    for(int i=1; i<=n; i++) if(indegree[i] == 0) q.push(i);
     
-    for(int i=1; i<=n; i++){
-        if(visit.find(i) != visit.end()) continue;
-        vector<int> arr; // 이번 사이클에 들어가는 노드
-        unordered_map<int,int> tmp;
-        f(i, arr, tmp);
-        for(auto k : arr) visit[k]++;
+    int not_in_cycle = 0;
+    
+    while(q.size()){
+        int h = q.front(); q.pop();
+        not_in_cycle++;
+        if(--indegree[pointer[h]] == 0) q.push(pointer[h]);
     }
 
-    cout << n - is_in_cycle.size() << '\n';
+    cout << not_in_cycle << '\n';
 
 }
 
